f608e1b59f37be4e764d2f05230d6e6513614f7d
Daniel Haddington <haddingtondaniel5@icloud.com>
2025-11-24 15:34:20 +1030

    fixed logic so that key up behaves like key down

--- source/key_input.c
+++ source/key_input.c
@@ -4,19 +4,23 @@
 
 void handle_up_key(t_c *c)
 {
-	if (c->vis_y == YOFFSET && c->scroll_yoffset != 0)
-		c->scroll_yoffset -= 1;
-	if (c->vis_y > YOFFSET)
-		c->vis_y -= 1;
 	if (c->current_line != 0)
 	{
 		c->current_line -= 1;
 		c->current_line_length =  check_line_length(c);
-	}
-		
-	
+		if (c->vis_x > c->current_line_length && c->vis_x > XOFFSET)
+		{
+			c->vis_x = c->current_line_length + XOFFSET;
+		} 
+	} else return ;
+
+	if (c->vis_y == YOFFSET && c->scroll_yoffset != 0)
+		c->scroll_yoffset -= 1;
+	if (c->vis_y > YOFFSET)
+		c->vis_y -= 1;
 
 }
+
 void handle_down_key(t_c *c)
 {
 	if (c->current_line < c->line_count)
@@ -25,7 +29,7 @@ void handle_down_key(t_c *c)
 		c->current_line_length = check_line_length(c);
 		if (c->vis_x > c->current_line_length && c->vis_x < c->max_x - 1)
 		{
-			c->vis_x = c->current_line_length;
+			c->vis_x = c->current_line_length + XOFFSET;
 		} 
 	} else return ;
 		
@@ -40,7 +44,7 @@ void handle_down_key(t_c *c)
 void handle_right_key(t_c *c)
 {
 	
-	if (c->vis_x < c->current_line_length)
+	if (c->vis_x < c->current_line_length + 2)
 	{
 		if (c->vis_x == c->max_x - 1)
 			c->scroll_xoffset += 1;
