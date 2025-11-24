23a06b1e9e0759b9e260a982855dc36d93f4c954
Daniel Haddington <haddingtondaniel5@icloud.com>
2025-11-24 15:34:36 +1030

    removed test to see cursor position

--- source/main.c
+++ source/main.c
@@ -58,7 +58,8 @@ int main(int argc, char **argv)
 		draw_buffer(c);
 		draw_pane(c->max_y, c->max_x);
 		attron(COLOR_PAIR(2));
-		mvprintw(c->vis_y, c->vis_x, "%d", c->current_line_length);
+		// mvprintw(c->vis_y, c->vis_x, "%d", c->current_line_length);
+		move(c->vis_y, c->vis_x);
 		refresh();
 
 		int key = getch();
