28639b4306ef6b8724ecd33ee906641fd2ba1f57
Daniel Haddington <haddingtondaniel5@icloud.com>
2025-11-24 13:05:20 +1030

    added line counting mechanism for future use

--- source/key_input.c
+++ source/key_input.c
@@ -2,25 +2,37 @@
 
 void handle_up_key(t_c *c)
 {
-	if (c->vis_y == YOFFSET && c->scroll_offset != 0)
-		c->scroll_offset -= 1;
+	if (c->vis_y == YOFFSET && c->scroll_yoffset != 0)
+		c->scroll_yoffset -= 1;
 	if (c->vis_y > YOFFSET)
 		c->vis_y -= 1;
+	if (c->current_line != 0)
+		c->current_line -= 1;
+
 }
 void handle_down_key(t_c *c)
 {
+	if (c->current_line < c->line_count)
+	{
+		c->current_line += 1;
+	} else return ;
+		
 	if (c->vis_y == c->max_y - 1)
-		c->scroll_offset += 1;
+		c->scroll_yoffset += 1;
 	if (c->vis_y < c->max_y - 1)
 		c->vis_y += 1;
+
 }
 void handle_right_key(t_c *c)
 {
+	// if (c->vis_x <= check_line_length(c->current_line))
 	if (c->vis_x < c->max_x - 1)
 		c->vis_x += 1;	
 }
 void handle_left_key(t_c *c)
 {
+	if (c->vis_x == XOFFSET && c->scroll_xoffset != 0)
+		c->scroll_xoffset -= 1;
 	if (c->vis_x > XOFFSET)
 		c->vis_x -= 1;	
 }
